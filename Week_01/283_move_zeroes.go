f7f78354d5af536da7d15ffa4ff4f3b1118cff0a
alex <304704787@qq.com>
2020-09-09 03:34:24 +0800

    移动0其他解法

--- Week_01/283_move_zeroes.go
+++ Week_01/283_move_zeroes.go
@@ -1,7 +1,7 @@
 package Week_01
 
 //283. 移动零
-func moveZeroes(nums []int) {
+func moveZeroes1(nums []int) {
 	//0的位置
 	left, right := -1, -1
 
@@ -15,6 +15,31 @@ func moveZeroes(nums []int) {
 			nums[index], nums[left] = 0, value
 			left++
 			right++
+		}
+	}
+}
+
+func moveZeroes2(nums []int) {
+	//需要交换的位置
+	pos := 0
+
+	for index, value := range nums {
+		if value != 0 {
+			nums[index], nums[pos] = nums[pos], nums[index]
+			pos++
+		}
+	}
+}
+
+func moveZeroes3(nums []int) {
+	//0的个数
+	count := 0
+
+	for index, value := range nums {
+		if value == 0 {
+			count++
+		} else if count > 0 {
+			nums[index-count], nums[index] = nums[index], 0
 		}
 	}
 }
