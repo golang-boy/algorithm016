a49922ceb4564f0935b97467c21f52f6d786d445
alex <304704787@qq.com>
2020-09-10 02:05:01 +0800

    是否有环

---
+++ Week_01/14_has_cycle.go
@@ -0,0 +1,16 @@
+package Week_01
+
+func hasCycle(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+	fast, slow := head.Next, head
+	for fast != nil && fast.Next != nil {
+		if fast == slow {
+			return true
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return false
+}
